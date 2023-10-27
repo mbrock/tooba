ef39cb17c39d338eb7d37a0bf2d6ec506a8d0a0f
Mikael Brockman <mikael@brockman.se>
2023-10-27 12:17:19 +0300

    fix lb rb

--- src/gamepad.ts
+++ src/gamepad.ts
@@ -186,8 +186,8 @@ export const gamepadNavigationHandler = (
   const buttonMap: ButtonMap = {
     0: "A",
     1: "B",
-    5: "LB",
-    6: "RB",
+    4: "LB",
+    5: "RB",
     12: "Up",
     13: "Down",
     14: "Left",
