000aa1c3e461e1514cd48005eb579edbd545dc94
Mikael Brockman <mikael@brockman.se>
2023-10-30 13:19:40 +0200

    color

--- src/features/files/FileGroupView.tsx
+++ src/features/files/FileGroupView.tsx
@@ -147,7 +147,7 @@ const VideoFileGroupView: React.FC<VideoFileGroupProps> = ({
     if (!date) return null
     const dateObj = new Date(date)
     return (
-      <p className="text-gray-500  dark:text-gray-400">
+      <p className="text-gray-500 dark:text-gray-400">
         {formatDateToSwedish(dateObj)}
       </p>
     )
